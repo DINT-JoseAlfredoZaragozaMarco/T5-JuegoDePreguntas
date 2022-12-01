b4c7c4a885148dd702b600429ed0c9caff223fbd
JOSÉ ALFREDO ZARAGOZA MARCO <joszarmar@alu.edu.gva.es>
2022-12-01 12:49:20 +0100

    C osas

---
+++ Juego de preguntas/Modelo/ServicioJSON.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego_de_preguntas.Modelo
+{
+    class ServicioJSON
+    {
+        public void GuardarPreguntasEnJSON(ObservableCollection<Preguntas> preguntas)
+        {
+            string personasJson = JsonConvert.SerializeObject(preguntas);
+            File.WriteAllText("./datos/personas.json", personasJson);
+        }
+
+        public ObservableCollection<Preguntas> CargarPreguntasDeJSON()
+        {
+            ObservableCollection<Preguntas> preguntas = new ObservableCollection<Preguntas>();
+
+            string personasJson = File.ReadAllText("./datos/personas.json");
+            preguntas = JsonConvert.DeserializeObject<ObservableCollection<Preguntas>>(personasJson);
+
+            return preguntas;
+        }
+    }
+}
---
+++ Juego de preguntas/VistasModelo/MainWindowVM.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Juego_de_preguntas.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Juego_de_preguntas.VistasModelo
+{
+    class MainWindowVM : ObservableObject
+    {
+        private ObservableCollection<Preguntas> preguntas;
+
+        private Preguntas preguntaActual;
+
+        public Preguntas PreguntaActual
+        {
+            get { return preguntaActual; }
+            set { SetProperty(ref preguntaActual, value); }
+        }
+
+        public MainWindowVM() 
+        {
+            
+        }
+    }
+}
